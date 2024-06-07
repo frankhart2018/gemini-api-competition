20884eccf51333f1ea3f62fcca681da29e6eafd1
Arpit <ak30062004@gmail.com>
2024-06-07 08:47:51 +0530

    Frontend setup completed, plus completed Signup and Login routes

---
+++ Frontend/gemini-chat-app/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthPageLoginClient from "./client";
+
+export default function page() {
+  return (
+    <div>
+      <AuthPageLoginClient />
+    </div>
+  );
+}
---
+++ Frontend/gemini-chat-app/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthPageSignupClient from "./client";
+
+export default function page() {
+  return (
+    <div>
+      <AuthPageSignupClient />
+    </div>
+  );
+}
