c670dc848c3045787b27589621b6232d751b1856
ar6625 <ar6625@srmist.edu.in>
2024-06-08 02:21:23 +0530

    feat: userprofile image section

--- Frontend/gemini-chat-app/app/userprofile/page.tsx
+++ Frontend/gemini-chat-app/app/userprofile/page.tsx
@@ -1,12 +1,18 @@
 import { Separator } from "@/components/ui/separator"
 import { ProfileForm } from "./profiile-form"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 
 export default function SettingsProfilePage() {
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">Profile</h3>
-        <p className="text-sm text-muted-foreground">
+        <Avatar className="w-16 h-16"> {/* Increase the size of the avatar */}
+            <AvatarImage  src=" " alt="random picture" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <h3 className="text-lg font-medium py-3 px-1">Profile</h3>
+        <p className="text-sm text-muted-foreground px-1">
           Something about the user.
         </p>
       </div>
