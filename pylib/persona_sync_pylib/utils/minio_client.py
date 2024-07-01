8dc3b9dce38201ba1ec81756c431f23b008ce4dd
frankhart2018 <sdhargithub@gmail.com>
2024-06-30 19:06:22 -0500

    Add minio client in pylib

---
+++ pylib/persona_sync_pylib/utils/minio_client.py
@@ -0,0 +1,36 @@
+from minio import Minio, S3Error
+
+from .environment import MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY
+
+
+class MinioClient:
+    def __init__(self) -> None:
+        self.__client = Minio(
+            endpoint=MINIO_ENDPOINT,
+            access_key=MINIO_ACCESS_KEY,
+            secret_key=MINIO_SECRET_KEY,
+            secure=False,
+        )
+
+    def make_bucket_if_not_exists(self, bucket_name: str) -> None:
+        if not self.__client.bucket_exists(bucket_name):
+            self.__client.make_bucket(bucket_name)
+
+    def upload_file(
+        self, bucket_name: str, object_name: str, file_path: str, content_type: str
+    ) -> None:
+        self.__client.fput_object(
+            bucket_name, object_name, file_path, content_type=content_type
+        )
+
+    def upload_file_if_not_exists(
+        self, bucket_name: str, object_name: str, file_path: str, content_type: str
+    ) -> None:
+        self.make_bucket_if_not_exists(bucket_name)
+        try:
+            self.__client.stat_object(bucket_name, object_name)
+        except S3Error:
+            self.upload_file(bucket_name, object_name, file_path, content_type)
+
+    def download_file(self, bucket_name: str, object_name: str, file_path: str) -> None:
+        self.__client.fget_object(bucket_name, object_name, file_path)
