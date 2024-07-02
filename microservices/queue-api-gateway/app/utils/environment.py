5f634fe1dc7542b45f8945234c9b29f7e6b209d0
frankhart2018 <sdhargithub@gmail.com>
2024-07-01 21:35:39 -0500

    Add endpoint to get last 10 chats

--- microservices/queue-api-gateway/app/utils/environment.py
+++ microservices/queue-api-gateway/app/utils/environment.py
@@ -6,6 +6,8 @@ USER_CHAT_QUEUE = os.getenv("USER_CHAT_QUEUE_NAME", "user-chat-system")
 
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "chat-agents")
 PROMPT_INPUTS_COLLECTION = os.getenv("PROMPT_INPUTS_COLLECTION", "prompt-inputs")
+CHAT_HISTORY_DB = os.getenv("CHAT_HISTORY_DB", "chat_history")
+CHAT_HISTORY_COLLECTION = os.getenv("CHAT_HISTORY_COLLECTION", "chats")
 
 SOCKET_SESSIONS_BUCKET = os.getenv("SOCKET_SESSIONS_BUCKET", "socket_sessions")
 SOCKET_SESSIONS_TMP_DIR = os.getenv("SOCKET_SESSIONS_TMP_DIR", "/tmp/socket_sessions")
